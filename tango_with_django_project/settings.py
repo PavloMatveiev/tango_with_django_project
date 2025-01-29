9a60ed99422ecb8772655b0d51df4db268f17f01
Pavlo Matveiev <matveievp@gmail.com>
2025-01-29 05:57:52 +0000

    add a template index.html to rango, add about page, link two pages about and index

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "rango" / "templates"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
